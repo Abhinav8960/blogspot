98d8990c38eb0e5a8a3f23053062ad263dab050d
Abhinav <abhinavpal8960@gmail.com>
2026-02-25 19:17:09 +0530

    change

--- resources/views/home/homecss.php
+++ resources/views/home/homecss.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="viewport" content="initial-scale=1, maximum-scale=1">
 
-<title>A World</title>
+<title>BlogSpot</title>
 
 <meta name="keywords" content="">
 <meta name="description" content="">
