b0787add74dcecfa7a1562973ecc9b666196b489
Abhinav <abhinavpal8960@gmail.com>
2026-03-18 23:39:48 +0530

    change in status for posts table migration

--- database/migrations/2026_01_30_203650_create_posts_table.php
+++ database/migrations/2026_01_30_203650_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->string('title')->nullable();
             $table->longText('description')->nullable();
             $table->string('image')->nullable();
-            $table->boolean('status')->default(1);
+            $table->tinyInteger('status')->default(1)->nullable(false)->change();
             $table->timestamps();
         });
     }
