4e8f9699f5586159e10249269c21a3f0d9bcfa93
Abhinav <abhinavpal8960@gmail.com>
2026-03-18 17:26:32 +0530

    fix https assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
 
-        URL::forceScheme('https');
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
         Paginator::useBootstrap();
     }
 }
