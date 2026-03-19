bba4292e15e55e47cc351472533dfa79d0c4f124
Abhinav <abhinavpal8960@gmail.com>
2026-03-19 22:54:39 +0530

    chnages

---
+++ database/migrations/2026_03_19_172352_fix_status_column_pgsql.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        if (DB::getDriverName() === 'pgsql') {
+
+            DB::statement('ALTER TABLE posts ALTER COLUMN status DROP DEFAULT');
+
+            DB::statement("
+                ALTER TABLE posts 
+                ALTER COLUMN status TYPE smallint 
+                USING (CASE WHEN status = true THEN 1 ELSE 0 END)
+            ");
+
+            DB::statement('ALTER TABLE posts ALTER COLUMN status SET DEFAULT 1');
+        }
+    }
+
+    public function down(): void
+    {
+        if (DB::getDriverName() === 'pgsql') {
+            DB::statement("
+                ALTER TABLE posts 
+                ALTER COLUMN status TYPE boolean 
+                USING (CASE WHEN status = 1 THEN true ELSE false END)
+            ");
+        }
+    }
+};
