b38668bdc819158f5a442308f13ece39097489da
Abhinav <abhinavpal8960@gmail.com>
2026-04-07 19:01:23 +0530

    fix

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -1,120 +1,117 @@
 <div class="page-header">
-                <div class="container-fluid">
-                    <h2 class="h5 no-margin-bottom">Dashboard</h2>
+    <div class="container-fluid">
+        <h2 class="h5 no-margin-bottom">Dashboard</h2>
+    </div>
+</div>
+<section class="no-padding-top no-padding-bottom">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="statistic-block block">
+                    <div class="progress-details d-flex align-items-end justify-content-between">
+                        <div class="title">
+                            <div class="icon"><i class="icon-user-1"></i></div><strong>New Clients</strong>
+                        </div>
+                        <div class="number dashtext-1">27</div>
+                    </div>
+                    <div class="progress progress-template">
+                        <div role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" class="progress-bar progress-bar-template dashbg-1"></div>
+                    </div>
                 </div>
             </div>
-            <section class="no-padding-top no-padding-bottom">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-3 col-sm-6">
-                            <div class="statistic-block block">
-                                <div class="progress-details d-flex align-items-end justify-content-between">
-                                    <div class="title">
-                                        <div class="icon"><i class="icon-user-1"></i></div><strong>New Clients</strong>
-                                    </div>
-                                    <div class="number dashtext-1">27</div>
-                                </div>
-                                <div class="progress progress-template">
-                                    <div role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" class="progress-bar progress-bar-template dashbg-1"></div>
-                                </div>
-                            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="statistic-block block">
+                    <div class="progress-details d-flex align-items-end justify-content-between">
+                        <div class="title">
+                            <div class="icon"><i class="icon-contract"></i></div><strong>New Projects</strong>
                         </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="statistic-block block">
-                                <div class="progress-details d-flex align-items-end justify-content-between">
-                                    <div class="title">
-                                        <div class="icon"><i class="icon-contract"></i></div><strong>New Projects</strong>
-                                    </div>
-                                    <div class="number dashtext-2">375</div>
-                                </div>
-                                <div class="progress progress-template">
-                                    <div role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" class="progress-bar progress-bar-template dashbg-2"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="statistic-block block">
-                                <div class="progress-details d-flex align-items-end justify-content-between">
-                                    <div class="title">
-                                        <div class="icon"><i class="icon-paper-and-pencil"></i></div><strong>New Invoices</strong>
-                                    </div>
-                                    <div class="number dashtext-3">140</div>
-                                </div>
-                                <div class="progress progress-template">
-                                    <div role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" class="progress-bar progress-bar-template dashbg-3"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="statistic-block block">
-                                <div class="progress-details d-flex align-items-end justify-content-between">
-                                    <div class="title">
-                                        <div class="icon"><i class="icon-writing-whiteboard"></i></div><strong>All Projects</strong>
-                                    </div>
-                                    <div class="number dashtext-4">41</div>
-                                </div>
-                                <div class="progress progress-template">
-                                    <div role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100" class="progress-bar progress-bar-template dashbg-4"></div>
-                                </div>
-                            </div>
-                        </div>
+                        <div class="number dashtext-2">375</div>
+                    </div>
+                    <div class="progress progress-template">
+                        <div role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" class="progress-bar progress-bar-template dashbg-2"></div>
                     </div>
                 </div>
-            </section>
-             
-           
-            <section class="no-padding-bottom">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-4">
-                            <div class="user-block block text-center">
-                                <div class="avatar"><img src="img/avatar-1.jpg" alt="..." class="img-fluid">
-                                    <div class="order dashbg-2">1st</div>
-                                </div><a href="#" class="user-title">
-                                    <h3 class="h5">Richard Nevoreski</h3><span>@richardnevo</span>
-                                </a>
-                                <div class="contributions">950 Contributions</div>
-                                <div class="details d-flex">
-                                    <div class="item"><i class="icon-info"></i><strong>150</strong></div>
-                                    <div class="item"><i class="fa fa-gg"></i><strong>340</strong></div>
-                                    <div class="item"><i class="icon-flow-branch"></i><strong>460</strong></div>
-                                </div>
-                            </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="statistic-block block">
+                    <div class="progress-details d-flex align-items-end justify-content-between">
+                        <div class="title">
+                            <div class="icon"><i class="icon-paper-and-pencil"></i></div><strong>New Invoices</strong>
                         </div>
-                        <div class="col-lg-4">
-                            <div class="user-block block text-center">
-                                <div class="avatar"><img src="img/avatar-4.jpg" alt="..." class="img-fluid">
-                                    <div class="order dashbg-1">2nd</div>
-                                </div><a href="#" class="user-title">
-                                    <h3 class="h5">Samuel Watson</h3><span>@samwatson</span>
-                                </a>
-                                <div class="contributions">772 Contributions</div>
-                                <div class="details d-flex">
-                                    <div class="item"><i class="icon-info"></i><strong>80</strong></div>
-                                    <div class="item"><i class="fa fa-gg"></i><strong>420</strong></div>
-                                    <div class="item"><i class="icon-flow-branch"></i><strong>272</strong></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="user-block block text-center">
-                                <div class="avatar"><img src="img/avatar-6.jpg" alt="..." class="img-fluid">
-                                    <div class="order dashbg-4">3rd</div>
-                                </div><a href="#" class="user-title">
-                                    <h3 class="h5">Sebastian Wood</h3><span>@sebastian</span>
-                                </a>
-                                <div class="contributions">620 Contributions</div>
-                                <div class="details d-flex">
-                                    <div class="item"><i class="icon-info"></i><strong>150</strong></div>
-                                    <div class="item"><i class="fa fa-gg"></i><strong>280</strong></div>
-                                    <div class="item"><i class="icon-flow-branch"></i><strong>190</strong></div>
-                                </div>
-                            </div>
-                        </div>
+                        <div class="number dashtext-3">140</div>
                     </div>
-       
+                    <div class="progress progress-template">
+                        <div role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" class="progress-bar progress-bar-template dashbg-3"></div>
+                    </div>
                 </div>
-            </section>
-             
-         
-        
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="statistic-block block">
+                    <div class="progress-details d-flex align-items-end justify-content-between">
+                        <div class="title">
+                            <div class="icon"><i class="icon-writing-whiteboard"></i></div><strong>All Projects</strong>
+                        </div>
+                        <div class="number dashtext-4">41</div>
+                    </div>
+                    <div class="progress progress-template">
+                        <div role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100" class="progress-bar progress-bar-template dashbg-4"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="no-padding-bottom">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="user-block block text-center">
+                    <div class="avatar"><img src="{{ asset('adminassets/img/avatar-1.jpg') }}" alt="..." class="img-fluid">
+                        <div class="order dashbg-2">1st</div>
+                    </div><a href="#" class="user-title">
+                        <h3 class="h5">Richard Nevoreski</h3><span>@richardnevo</span>
+                    </a>
+                    <div class="contributions">950 Contributions</div>
+                    <div class="details d-flex">
+                        <div class="item"><i class="icon-info"></i><strong>150</strong></div>
+                        <div class="item"><i class="fa fa-gg"></i><strong>340</strong></div>
+                        <div class="item"><i class="icon-flow-branch"></i><strong>460</strong></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="user-block block text-center">
+                    <div class="avatar"><img src="{{ asset('adminassets/img/avatar-4.jpg') }}" alt="..." class="img-fluid">
+                        <div class="order dashbg-1">2nd</div>
+                    </div><a href="#" class="user-title">
+                        <h3 class="h5">Samuel Watson</h3><span>@samwatson</span>
+                    </a>
+                    <div class="contributions">772 Contributions</div>
+                    <div class="details d-flex">
+                        <div class="item"><i class="icon-info"></i><strong>80</strong></div>
+                        <div class="item"><i class="fa fa-gg"></i><strong>420</strong></div>
+                        <div class="item"><i class="icon-flow-branch"></i><strong>272</strong></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="user-block block text-center">
+                    <div class="avatar"><img src="{{ asset('adminassets/img/avatar-6.jpg') }}" alt="..." class="img-fluid">
+                        <div class="order dashbg-4">3rd</div>
+                    </div><a href="#" class="user-title">
+                        <h3 class="h5">Sebastian Wood</h3><span>@sebastian</span>
+                    </a>
+                    <div class="contributions">620 Contributions</div>
+                    <div class="details d-flex">
+                        <div class="item"><i class="icon-info"></i><strong>150</strong></div>
+                        <div class="item"><i class="fa fa-gg"></i><strong>280</strong></div>
+                        <div class="item"><i class="icon-flow-branch"></i><strong>190</strong></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
--- resources/views/admin/js.blade.php
+++ resources/views/admin/js.blade.php
@@ -1,9 +1,9 @@
-<script src="adminassets/vendor/jquery/jquery.min.js"></script>
-<script src="adminassets/vendor/popper.js/umd/popper.min.js"> </script>
-<script src="adminassets/vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="adminassets/vendor/jquery.cookie/jquery.cookie.js"> </script>
-<script src="adminassets/vendor/chart.js/Chart.min.js"></script>
-<script src="adminassets/vendor/jquery-validation/jquery.validate.min.js"></script>
-<script src="adminassets/js/charts-home.js"></script>
-<script src="adminassets/js/front.js"></script>
+<script src="{{ asset('adminassets/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('adminassets/vendor/popper.js/umd/popper.min.js') }}"> </script>
+<script src="{{ asset('adminassets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('adminassets/vendor/jquery.cookie/jquery.cookie.js') }}"> </script>
+<script src="{{ asset('adminassets/vendor/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('adminassets/vendor/jquery-validation/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('adminassets/js/charts-home.js') }}"></script>
+<script src="{{ asset('adminassets/js/front.js') }}"></script>
 <script src="{{ asset('adminassets/js/admin.js') }}"></script>
